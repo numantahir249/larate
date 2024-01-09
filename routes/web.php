f5fd4423b2483568f53b39b10b63a669545e7e41
numan <numantahir249@gmail.com>
2024-01-10 02:05:19 +0500

    fixed error

--- routes/web.php
+++ routes/web.php
@@ -34,16 +34,18 @@ Route::put('/form', function (Request $request) {
         'content' => ['required'],
     ]);
 
+
+
     Message::create([
         'name' => $request->input('name'),
         'email' => $request->input('email'),
         'content' => $request->input('content'),
     ]);
 
-    return redirect()->route('thank-you-page');
+    return redirect()->route('thank-you');
 
 })->name('form');
 
 Route::get('/thank-you', function () {
-    return view('thankyou');
+    return view('thank-you');
 })->name('thank-you');
